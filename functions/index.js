1f00ab08c88e66343779009b5fd0a7b98bcbdec2
Tanxy Rogue <tanxyrogue@gmail.com>
2019-08-09 16:42:46 +0200

    create copy to new function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
+/*jshint esversion: 6 */
+
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +10,14 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.copyToNew = functions.firestore
+    .document(`products/{productId}`)
+    .onCreate((snap) =>{
+        const newDocument = snap.data();
+
+        console.log(newDocument);
+
+        return admin.firestore().doc(`/new/${snap.id}`)
+                .set(newDocument);
+    });
