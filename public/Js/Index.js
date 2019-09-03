b717a62699fa7663c5723ce15ae680c610776578
Tanxy Rogue <tanxyrogue@gmail.com>
2019-09-03 19:25:22 +0200

    added tags input and fixed some styling issues

--- public/Js/Index.js
+++ public/Js/Index.js
@@ -14,6 +14,7 @@ const productCategoryInput = document.querySelector('#product-category');
 const productBrandInput = document.querySelector('#product-brand');
 const productPriceInput = document.querySelector('#product-price');
 const productDescriptionInput = document.querySelector('#product-description');
+const productTagsInput = document.querySelector('#product-tags');
 
 const browseBtn = document.querySelector('#browse-btn');
 const fileInfo = document.querySelector('#file-info');
@@ -68,6 +69,7 @@ function uploadDocument(){
         Brand: productBrandInput.value,
         Category: productCategoryInput.value,
         Description: productDescriptionInput.value,
+        Tags: productTagsInput.value.split(/[ .,]+/),
         UploadTimestamp: firebase.firestore.Timestamp.now().seconds
     })
     .then(function (docRef) {
